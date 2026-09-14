abd5a4020f48f94378213e112c74db0e1909e450
romini1993 <83305908+romini1993@users.noreply.github.com>
2026-09-14 10:27:58 +0700

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'hutang-pwa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
